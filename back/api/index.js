844dd8331b68eb5805afe95fade8d1d5357c95f6
LucasSan1 <evangelistalucas034@gmail.com>
2026-01-28 22:29:50 -0300

    nova tentativa

--- back/api/index.js
+++ back/api/index.js
@@ -1,15 +1,8 @@
 const serverless = require("serverless-http");
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-
-app.get("/", (req, res) => {
-  res.json({ message: "Estou vivo!" });
-});
+app.get("/", (req, res) => res.json({ message: "Estou vivo!" }));
 
 module.exports.handler = serverless(app);
