4502aea061714a8e7bec01411f8f8707f0756897
LucasSan1 <evangelistalucas034@gmail.com>
2026-01-28 22:24:23 -0300

    nova tentativa

--- back/api/index.js
+++ back/api/index.js
@@ -1,35 +1,15 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const rotas_reg = require("./routes");
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = "mongodb+srv://teste:TESTE222@meubanco.drgd8c5.mongodb.net/COMPROVANTE";
-let cachedConn = null;
 
-app.use(async (req, res, next) => {
-  try {
-    if (!cachedConn) {
-      cachedConn = await mongoose.connect(MONGO_URI, { dbName: "COMPROVANTE" });
-      console.log("Conectado ao MongoDB");
-    }
-    next();
-  } catch (err) {
-    console.error("Erro MongoDB:", err);
-    res.status(500).json("Erro interno do servidor (MongoDB)");
-  }
+app.get("/", (req, res) => {
+  res.json({ message: "Estou vivo!" });
 });
-
-app.use("/registros", rotas_reg);
 
 module.exports.handler = serverless(app);
